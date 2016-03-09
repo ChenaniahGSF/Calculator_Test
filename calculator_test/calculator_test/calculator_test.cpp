d90e2267e91a9181e9734eb26349db3bea8ca212
ChenaniahGSF <yeyinggsf@gmail.com>
2016-03-09 23:56:37 +0800

    增加了单元测试部分，程序未运行

--- calculator_test/calculator_test/calculator_test.cpp
+++ calculator_test/calculator_test/calculator_test.cpp
@@ -2,10 +2,30 @@
 //
 
 #include "stdafx.h"
+#include <gtest/gtest.h>
+#include "calculator.h"
+
+Calculator Bob;
+
+TEST(CalTest, AddMinusMultiplyDivide)
+{
+	EXPECT_DOUBLE_EQ(15, Bob.add(5, 10));
+	EXPECT_DOUBLE_EQ(25, Bob.add(7, 18));
+
+	EXPECT_DOUBLE_EQ(90, Bob.add(100, 10));
+	EXPECT_DOUBLE_EQ(-5, Bob.add(5, 10));
+
+	EXPECT_DOUBLE_EQ(54, Bob.add(6, 9));
+	EXPECT_DOUBLE_EQ(121, Bob.add(11, 11));
+
+	EXPECT_DOUBLE_EQ(5, Bob.add(10, 2));
+	EXPECT_DOUBLE_EQ(11, Bob.add(55, 5));
+	
+}
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	return 0;
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
-
