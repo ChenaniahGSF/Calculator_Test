faf3ac27cca6f4b94e1357457a785b46b5cccdb6
ChenaniahGSF <yeyinggsf@gmail.com>
2016-03-09 23:45:32 +0800

    定义了一个计算器类的头文件，用于接受测试

---
+++ calculator_test/calculator_test/calculator.h
@@ -0,0 +1,8 @@
+class Calculator
+{
+public:
+	double add(double i, double j){ return i + j; }
+	double minus(double i, double j){ return i - j; }
+	double multiply(double i, double j){ return i * j; }
+	double divide(double i, double j){ return i / j; }
+};
