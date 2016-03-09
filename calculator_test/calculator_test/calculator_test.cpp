17ff35ba275702e5c8320ce121294e0031c22321
ChenaniahGSF <yeyinggsf@gmail.com>
2016-03-10 00:02:43 +0800

    程序运行，结果出来，附上1.png

--- calculator_test/calculator_test/calculator_test.cpp
+++ calculator_test/calculator_test/calculator_test.cpp
@@ -12,14 +12,14 @@ TEST(CalTest, AddMinusMultiplyDivide)
 	EXPECT_DOUBLE_EQ(15, Bob.add(5, 10));
 	EXPECT_DOUBLE_EQ(25, Bob.add(7, 18));
 
-	EXPECT_DOUBLE_EQ(90, Bob.add(100, 10));
-	EXPECT_DOUBLE_EQ(-5, Bob.add(5, 10));
+	EXPECT_DOUBLE_EQ(90, Bob.minus(100, 10));
+	EXPECT_DOUBLE_EQ(-5, Bob.minus(5, 10));
 
-	EXPECT_DOUBLE_EQ(54, Bob.add(6, 9));
-	EXPECT_DOUBLE_EQ(121, Bob.add(11, 11));
+	EXPECT_DOUBLE_EQ(54, Bob.multiply(6, 9));
+	EXPECT_DOUBLE_EQ(121, Bob.multiply(11, 11));
 
-	EXPECT_DOUBLE_EQ(5, Bob.add(10, 2));
-	EXPECT_DOUBLE_EQ(11, Bob.add(55, 5));
+	EXPECT_DOUBLE_EQ(5, Bob.divide(10, 2));
+	EXPECT_DOUBLE_EQ(11, Bob.divide(55, 5));
 	
 }
 
